f0e060b5fff1e0f3d47c599f5e58dedfa6bb81a1
SzymonSzmigielski <szmigielski.szymon13@gmail.com>
2022-04-16 22:14:45 +0200

    final

--- src/main/java/pl/edu/mimuw/Program.java
+++ src/main/java/pl/edu/mimuw/Program.java
@@ -1,5 +1,24 @@
 package pl.edu.mimuw;
 
 public class Program {
+    private final String faculty;
+    private final String major;
     
+    public Program(String faculty, String major) {
+        this.faculty = faculty;
+        this.major = major;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    @Override
+    public String toString() {
+        return major + " at " + faculty;
+    }
 }
--- src/main/java/pl/edu/mimuw/Staff.java
+++ src/main/java/pl/edu/mimuw/Staff.java
@@ -1,9 +1,65 @@
 package pl.edu.mimuw;
-import java.util.Set;
-import java.util.List;
 
-public class Staff extends Person{
-    private String position;
-    private Set<Publication> publications;
-    private List<Program> programs;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Staff extends Person {
+
+  private String position;
+  private Set<Publication> publications;
+
+  /**
+   * constructor used mainly for copying
+   */
+  public Staff(String number, String firstName, String lastName, String position, Set<Publication> publications) {
+    super(number, firstName, lastName);
+    this.position = position;
+    this.publications = publications;
+  }
+
+  public Staff(String number, String firstName, String lastName, String position) {
+    super(number, firstName, lastName);
+    this.position = position;
+    this.publications = new HashSet<Publication>();
+  }
+
+  @Override
+  public String fullName() {
+    return position + " " + super.fullName();
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb
+      .append("I'm ")
+      .append(fullName())
+      .append(" ")
+      .append("number: ")
+      .append(number)
+      .append("\n")
+      .append("I published:");
+    for (Publication publication : publications) {
+      sb.append("\n").append(publication.toString());
+    }
+    sb.append("\n");
+    return sb.toString();
+  }
+
+  @Override
+  public Person createCopy() {
+    var copyOfPublications = new HashSet<Publication>();
+    for (Publication publication : publications) {
+      copyOfPublications.add(publication);
+    }
+    return new Staff(number, firstName, lastName, position, copyOfPublications);
+  }
+
+  @Override
+   /**
+   * Publications are non-mutable - no need for copying
+   */
+  public void addPublication(Publication publication) {
+    publications.add(publication);
+  }
 }
