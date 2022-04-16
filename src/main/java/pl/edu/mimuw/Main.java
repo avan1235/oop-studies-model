7690d0c07ddd8da892feed3172d435052ab39809
Scharmi <scharmi1500@gmail.com>
2022-04-16 18:59:05 +0200

    Test Commit 2

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -5,6 +5,6 @@ public class Main {
   public static void main(String[] args) {
     // TODO: play with your students and staff members and show how
     //  they describe themselves
-    // test commit
+    // test commit 2
   }
 }
