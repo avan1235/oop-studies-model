7613e61bcc2ab406d5fd752c83b37e727e90c6fc
SMrozicki2137 <s.mrozicki123@gmail.com>
2022-04-16 22:04:24 +0200

    praca domowa

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -3,8 +3,6 @@ package pl.edu.mimuw;
 public class Main {
 
   public static void main(String[] args) {
-    // TODO: play with your students and staff members and show how
-    //  they describe themselves
     var mim = new USOS("MIMUW USOS");
     var dean = PersonFactory.createStaffMember("Paweł", "Strzelecki", "male", "12345678901");
     dean.WritePublication(new Publication("Analiza matematyczna I (skrypt wykładu)", "mathematical analysis"));
