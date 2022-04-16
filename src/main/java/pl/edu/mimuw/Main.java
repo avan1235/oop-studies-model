8e36a3c6ceedb63af9b80377b698f1d29e82ecda
SMrozicki2137 <s.mrozicki123@gmail.com>
2022-04-16 22:02:15 +0200

    praca domowa

--- src/main/java/pl/edu/mimuw/Main.java
+++ src/main/java/pl/edu/mimuw/Main.java
@@ -5,5 +5,19 @@ public class Main {
   public static void main(String[] args) {
     // TODO: play with your students and staff members and show how
     //  they describe themselves
+    var mim = new USOS("MIMUW USOS");
+    var dean = PersonFactory.createStaffMember("Paweł", "Strzelecki", "male", "12345678901");
+    dean.WritePublication(new Publication("Analiza matematyczna I (skrypt wykładu)", "mathematical analysis"));
+    mim.addStaffMember(dean);
+    var student1=PersonFactory.createStudent("Mateusz",  "Scharmach", "male", "02137696969", new Program(1, "MIM", "JSIM"));
+    var student2=PersonFactory.createStudent("Krzysztof", "Wolicki", "male", "21154206969", new Program(2, "MISMAP", "Physics"));
+    var student3=PersonFactory.createStudent("Michał", "Trzebuchowski", "male", "42021373180");
+    student2.addProgram(new Program(1, "MISMAP", "Informatics"));
+    student3.addProgram(new Program(1, "MIM", "Mathematics"));
+    mim.addStudent(student1);
+    mim.addStudent(student2);
+    mim.addStudent(student3);
+    mim.incremetYears();
+    System.out.println(mim.toString());
   }
 }
