c54830a738dd1558c259343c886753f50a439e0a
Pa_jacek <jacek.muszynski.mail@gmail.com>
2022-04-23 21:05:32 +0200

    tak solution

---
+++ src/main/java/pl/edu/mimuw/usos/People/Student.java
@@ -0,0 +1,34 @@
+package pl.edu.mimuw.usos.People;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends Person {
+  private final List<Integer> programsIds;
+  private String studyStatus;
+
+  public Student(String firstName, String secondName, String birthDate, String studyStatus) {
+    super(firstName, secondName, birthDate);
+    this.programsIds = new ArrayList<Integer>();
+    this.studyStatus = studyStatus;
+  }
+
+  public void addProgram(Integer programId){
+    this.programsIds.add(programId);
+  }
+
+  public void changeStatus(String status){
+    this.studyStatus = status;
+  }
+
+  public String getStudyStatus(){
+    return this.studyStatus;
+  }
+  public ArrayList<Integer> getProgramsIds(){
+    return new ArrayList<Integer>(this.programsIds);
+  }
+
+  public String studentDetailedInfo(){
+    return basicInfo() + ", has status: " + studyStatus;
+  }
+}
